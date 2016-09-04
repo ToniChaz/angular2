91d493b5872e1c4dfa86734948fead0efe6257e9
Toni Chaz <toni.chaz@hotmail.com>
2016-09-04 21:26:46 +0200

    Add heroes, multiple components, services and routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,31 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }   from './app.component';
+import { routing }             from './app.routing';
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap: [ AppComponent ]
 })
 
-export class AppModule { }
+export class AppModule {
+}
